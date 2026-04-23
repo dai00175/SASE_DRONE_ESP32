e28a43735507c37204f6fcbbcde5476cb522aa65
nguy4803 <nguy4803@umn.edu>
2026-04-23 18:38:41 -0500

    Fix kevin bad code im better

--- main/bluetooth_link.cpp
+++ main/bluetooth_link.cpp
@@ -236,7 +236,6 @@ esp_err_t init()
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
         .rx_flow_ctrl_thresh = 0,
-        .rx_glitch_filt_thresh = 0,
         .source_clk = UART_SCLK_DEFAULT,
         .flags = {},
     };
